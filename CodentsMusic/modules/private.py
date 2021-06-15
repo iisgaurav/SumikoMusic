912d00eb9c0e80bdf53e6b060c6e03576e87c1ec
Gaurav <gauravv2504@gmail.com>
2021-06-16 01:30:07 +0530

    Update private.py

--- CodentsMusic/modules/private.py
+++ CodentsMusic/modules/private.py
@@ -93,7 +93,6 @@ def map(pos):
             [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
             [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
              InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/{SUPPORT_GROUP}")],
-            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
